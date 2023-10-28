a6beb7995302fbc455652a02cf65c1b2671bb7bc
Dawnyson <127817052+dawnyson@users.noreply.github.com>
2023-10-28 13:48:56 +0700

    Add files via upload

---
+++ bracket_check.py
@@ -0,0 +1,34 @@
+from Stack import Stack
+
+# input: str
+# output: is_error : bool
+# output: location : int
+
+def bracket_check(str):
+    is_error = False
+    location = []
+    stack = Stack()
+
+    for i in range(len(str)):
+        s = str[i]
+        if s == '(' or s == '[' or s == '{':
+            stack.push(s)
+        elif s == ')' or s == ']' or s == '}':
+            if not stack.isEmpty():
+                p = stack.pop()
+
+                if not ((p == '(' and s == ')') or (p == '[' and s == ']') or (p == '{' and s == '}')):
+                    is_error = True
+                    location.append(i)
+            else:
+                is_error = True
+                location.append(i)
+
+    if not stack.isEmpty():
+        is_error = True
+        location.append(i)
+
+    print(is_error, location)
+    return is_error, location
+
+bracket_check('{ass{][(}}{(')
